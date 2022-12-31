85855840ae623a89d2324a782c2fd01075a45b00
Mohammed <moh9_kamal@hotmail.com>
2022-12-31 23:02:54 +0200

    update

--- 5.4/5.4.py
+++ 5.4/5.4.py
@@ -1,14 +1,19 @@
 
-
+# EX1
 tuple1=(10,20,30,40,50)
 
 tuple1=tuple(reversed(tuple1))
 
 print(tuple1)
 
+# EX2
+
 tuple2 = ('orange',[10,20,30],(5,15,25))
 
 print(tuple2[1][1])
+
+
+# EX3
 
 tuple3 =(10,20,30,40)
 
