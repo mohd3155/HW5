8be232838e451be371e4110378eb84c14e55333c
Mohammed <moh9_kamal@hotmail.com>
2022-12-31 23:01:56 +0200

    update

--- 5.3/5.3.py
+++ 5.3/5.3.py
@@ -1,8 +1,12 @@
 
+#EX1
+
 sample_set={'yellow','orange','black'}
 sample_list=['blue','green','red']
 
 print(sample_set.union(sample_list))
+
+# EX2
 
 set1={10,20,30,40,50}
 set2={30,40,50,60,70}
